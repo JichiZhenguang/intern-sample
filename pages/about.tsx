44824c8168599f38c8732913653e9c67b543ffb2
Mahiro Yoshimochi <mhr.ysmc1360@gmail.com>
2022-06-04 21:59:34 +0900

    add new file

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+const About: NextPage = () => {
+    return(
+        <div>
+            <h1>
+                about
+            </h1>
+            <p>intern</p>
+        </div>
+    )
+}
+
+export default About
